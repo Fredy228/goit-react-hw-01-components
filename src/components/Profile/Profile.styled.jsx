0a8831e5f3251b0958c200cab97203da95bccb30
Fredy228 <smagrovich58@gmail.com>
2022-12-21 22:53:26 +0200

    start-profile

---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled'
+
+export const Profle = styled.div`
+    
+`;
+export const Description = styled.div`
+    
+`;
+export const Avatar = styled.img`
+    
+`;
+export const Name = styled.p`
+    
+`;
+export const Text = styled.p`
+    
+`;
+
+export const Stats = styled.ul`
+    
+`;
+export const StatsItem = styled.li`
+    
+`;
+
+export const Label = styled.span`
+    
+`;
+export const Quantity = styled.span`
+    
+`;
+
